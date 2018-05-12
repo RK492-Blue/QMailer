ff9fa9592fbd476361d7dc39f4487d4f6daf3cab
Ricky <ricky@Rickys-MacBook-Pro.local>
2018-05-13 02:20:55 +1000

    reversed prev. commit

--- config/keys.js
+++ config/keys.js
@@ -1,15 +1,9 @@
 // keys.js - figure out what set of credentials to return
-// if(process.env.NODE_ENV === 'production'){
-//     // production environment - return prod keys
-//     module.exports = require('./prod');
-// } else {
-//     // dev environment - return dev keys
-//     module.exports = require('./dev');
-// }
+if(process.env.NODE_ENV === 'production'){
+    // production environment - return prod keys
+    module.exports = require('./prod');
+} else {
+    // dev environment - return dev keys
+    module.exports = require('./dev');
+}
 
-module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
-};
